96ec06cc83de7060b01afdc1440d159622f768fa
Tish Faroul <lfaroul14@gmail.com>
2022-05-23 22:43:11 -0400

    working on routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import Footer from './Footer';
 import Header from './Header';
 import Home from './Home';
+import About from "./About";
+import Search from './Search';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
     return ( 
         <div className = "app" >
-
      <Header/>
-        <Home/>
-        
 
+         <Router>
+             
+    
+            <Routes>
 
+                <Route path="/" element={ <Home/>} />
+                 
+                <Route path="/search" element={<Search />} />
 
+                <Route path="/about" element={<About />} />
+            
+            </Routes>
+  
+         </Router>
 
-        <Footer />
+     <Footer />
+
         </div>
     );
 }
